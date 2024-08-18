7e6da33f40986617b229e912402d564e039f14f3
piotryfon <pit.pit@wp.pl>
2024-08-18 09:04:07 +0200

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,8 +14,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+    
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
